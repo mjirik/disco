a0e59ceebe74ce00c033881c4d080f4b79f367ff
Miroslav Jiřík <miroslav.jirik@gmail.com>
2019-01-29 09:47:33 +0100

    long description content type

--- discon/__main__.py
+++ discon/__main__.py
@@ -229,6 +229,7 @@ setup(
     name='{name}',
     description='{description}',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
