71e2ba64c52e6289c172bdfd831fd7ca4169f611
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-09-10 10:30:11 +0200

    loguru used

--- discon/__main__.py
+++ discon/__main__.py
@@ -13,9 +13,9 @@ There is also option to init new project directory.
 
 """
 
-import logging
-
-logger = logging.getLogger(__name__)
+# import logging
+# logger = logging.getLogger(__name__)
+from loguru import logger
 import argparse
 import subprocess
 import os
