02f3f30656a096f8d4c65b15ced575e35366cc18
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-09-07 10:34:16 +0200

    install not only on travis

--- tools/install_conda.sh
+++ tools/install_conda.sh
@@ -17,8 +17,21 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda.exe;
 else
-    echo "Error: Just for use on travis"
+#    echo "Error: Just for use on travis"
+    if [ "$(uname)" == "Darwin" ]; then
+        # Do something under Mac OS X platform
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        # Do something under GNU/Linux platform
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        # Do something under 32 bits Windows NT platform
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda.exe;
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+        # Do something under 64 bits Windows NT platform
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -O miniconda.exe;
+    fi
 fi
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda3
+export PATH="$HOME/miniconda3/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
